35131d0dd8e8a6b36d2e3f1a8c8d105790fe64e3
Amir Musaev <a.musaev@centroidsol.com>
2023-04-17 17:04:14 +0400

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,85 @@
-import React from 'react';
-import { Box, Button, Paper, Typography } from '@mui/material';
+import React from "react";
+import { Box, Button, Paper, TextField, Typography } from "@mui/material";
 
 export const App: React.FC = () => {
   return (
-    <>
-      <Box paddingX={20} paddingY={4}>
-        <Typography component="span" sx={{ color: '#0055B9' }} fontSize={40} fontWeight={900}>T</Typography>
-        <Typography component="span" sx={{ color: '#3F3F3F' }} fontSize={40} fontWeight={900}>Data конвертер</Typography>
+    <Box maxWidth={1240} marginX="auto">
+      <Box
+        paddingX={{ xs: 2, md: 4 }}
+        height={90}
+        display="flex"
+        alignItems="center"
+      >
+        <Typography
+          component="span"
+          sx={{ color: "#0055B9" }}
+          fontSize={{ xs: 30, md: 40 }}
+          fontWeight={900}
+        >
+          TD
+        </Typography>
+        <Typography
+          component="span"
+          sx={{ color: "#3F3F3F" }}
+          fontSize={{ xs: 30, md: 40 }}
+          fontWeight={900}
+        >
+          ata конвертер
+        </Typography>
       </Box>
-      <Box paddingX={20}>
-        <Typography fontSize={18}>Перетащите архивы с tdata сюда</Typography>
-        <Box display="flex" alignItems="center" gap={20}>
-          <Box>
-            <Box width={600} height={250} border="3px solid rgba(0, 97, 210, 1)" borderRadius={3} marginTop={2}></Box>
-            <Box width={600} height={250} border="3px solid rgba(0, 97, 210, 1)" borderRadius={3} marginTop={2}></Box>
-          </Box>
-          <Box display="flex" flexDirection="column" alignItems="center" gap={4}>
-            <Button sx={{ width: 270, height: 145, borderRadius: 70, background: '#0061D2 !important', color: 'white', fontSize: 56, fontWeight: 'bold' }} variant="text">GO!</Button>
-            <Paper sx={{ width: 100, height: 100 }}></Paper>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Box>
+          <Typography fontSize={16}>Перетащите архивы с tdata сюда</Typography>
+          <Box
+            display="flex"
+            alignItems="center"
+            gap={5}
+            flexDirection={{ xs: "column", md: "row" }}
+          >
+            <Box>
+              <TextField
+                multiline
+                rows={7}
+                sx={{
+                  width: 500,
+                  maxWidth: "90vw",
+                  border: "3px solid rgba(0, 97, 210, 1)",
+                  borderRadius: 3,
+                  marginTop: 2,
+                  fieldset: {
+                    border: "none",
+                  },
+                }}
+              />
+              <Box
+                width={500}
+                maxWidth="90vw"
+                height={200}
+                border="3px solid rgba(0, 97, 210, 1)"
+                borderRadius={3}
+                marginTop={2}
+              ></Box>
+            </Box>
+            <Box display="flex" alignItems="center" gap={2}>
+              <Button
+                sx={{
+                  width: 120,
+                  height: 80,
+                  borderRadius: 4,
+                  background: "#0061D2 !important",
+                  color: "white",
+                  fontSize: 36,
+                  fontWeight: "bold",
+                }}
+                variant="text"
+              >
+                GO!
+              </Button>
+            </Box>
           </Box>
         </Box>
       </Box>
       <Box></Box>
-    </>
+    </Box>
   );
-}
-
+};
