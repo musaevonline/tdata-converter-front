59fc9e1e8dd005fae30aa4d643cd93d4c8493bc4
Amir Musaev <a.musaev@centroidsol.com>
2023-04-18 14:28:27 +0400

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import React, { useState } from "react";
-import { Box, Button, TextField, Typography } from "@mui/material";
-import { useDropzone } from 'react-dropzone'
+import { Box, Button, IconButton, TextField, Typography } from "@mui/material";
+import { useDropzone } from "react-dropzone";
+import ClearIcon from "@mui/icons-material/Clear";
 
 export const App: React.FC = () => {
   const [files, setFiles] = useState<File[]>([]);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    maxFiles: 10, onDrop(accepted) {
-      setFiles([...files, ...accepted])
-    }
-  })
+    maxFiles: 10,
+    onDrop(accepted) {
+      const fileNames = files.map((file) => file.name);
+      const newFiles = accepted.filter(
+        (file) => !fileNames.includes(file.name)
+      );
+      setFiles([...files, ...newFiles]);
+    },
+  });
 
   return (
     <Box maxWidth={1240} marginX="auto">
-      <Box
-        height={90}
-        display="flex"
-        alignItems="center"
-      >
+      <Box height={90} display="flex" alignItems="center">
         <Typography
           component="span"
           sx={{ color: "#0055B9" }}
@@ -35,11 +37,7 @@ export const App: React.FC = () => {
         </Typography>
       </Box>
       <Box display="flex" gap={10}>
-        <Box
-          display="flex"
-          gap={2}
-          flexDirection="column"
-        >
+        <Box display="flex" gap={2} flexDirection="column">
           <Box
             {...getRootProps()}
             width={500}
@@ -48,25 +46,53 @@ export const App: React.FC = () => {
             border="3px solid rgba(0, 97, 210, 1)"
             borderRadius={3}
             display="flex"
-            flexDirection="column"
             justifyContent="center"
             alignItems="center"
+            padding={2}
           >
             <input {...getInputProps()} />
-            {
-              files.length ? (
-                <Box sx={{ overflowY: 'auto' }}>
-                  {files.map(file => <Typography>{file.name}</Typography>)}
-                </Box>
-              ) : isDragActive ? (
-                <Typography>Ловлю... Ловлю...</Typography>
-              ) : (
-                <>
-                  <Box component="img" src="/dnd.png" />
-                  <Typography>Перетащите архивы с tdata в это поле</Typography>
-                </>
-              )
-            }
+
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              flexBasis="40%"
+            >
+              <Box component="img" src="/dnd.png" />
+              <Typography textAlign="center" color="#a9a9a9">
+                {isDragActive
+                  ? "Ловлю..."
+                  : "Перетащите архивы с tdata в это поле"}
+              </Typography>
+            </Box>
+
+            {!!files.length && (
+              <Box
+                overflow="hidden auto"
+                alignSelf="flex-start"
+                flexBasis="60%"
+                height="100%"
+                flexGrow={1}
+              >
+                {files.map((file) => (
+                  <Box display="flex" gap={0.5}>
+                    <IconButton
+                      sx={{ width: 24, height: 24 }}
+                      color="error"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setFiles(
+                          files.filter((currentFile) => currentFile !== file)
+                        );
+                      }}
+                    >
+                      <ClearIcon color="error" />
+                    </IconButton>
+                    <Typography textOverflow="ellipsis">{file.name}</Typography>
+                  </Box>
+                ))}
+              </Box>
+            )}
           </Box>
           <TextField
             placeholder="Сюда можете добавить ссылки на яндекс/mega/drive облака"
@@ -86,7 +112,7 @@ export const App: React.FC = () => {
             sx={{
               width: 200,
               height: 40,
-              alignSelf: 'center',
+              alignSelf: "center",
               borderRadius: 2,
               background: "#0061D2 !important",
               color: "white",
@@ -97,11 +123,28 @@ export const App: React.FC = () => {
             Конвертировать
           </Button>
         </Box>
-        <Typography>What is Lorem Ipsum?
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software.
-          Why do we use it?<br/><br/>
-
-          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</Typography>
+        <Typography>
+          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
+          and typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book. It has survived
+          not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software. Why do
+          we use it?
+          <br />
+          <br />
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).
+        </Typography>
       </Box>
     </Box>
   );
--- src/myTheme.tsx
+++ src/myTheme.tsx
@@ -1,17 +1,26 @@
 import { createTheme } from "@mui/material";
 
 export const myTheme = createTheme({
-    typography: {
-        fontFamily: "'Montserrat', sans-serif",
+  typography: {
+    fontFamily: "'Montserrat', sans-serif",
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        contained: {
+          textTransform: "initial",
+          boxShadow: "none !important;",
+        },
+      },
     },
-    components: {
-        MuiButton: {
-            styleOverrides: {
-                'contained': {
-                    textTransform: 'initial',
-                    boxShadow: 'none !important;',
-                }
-            }
-        }
-    }
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          "::placeholder": {
+            color: "#a9a9a9",
+          },
+        },
+      },
+    },
+  },
 });
